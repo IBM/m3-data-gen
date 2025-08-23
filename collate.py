702ee4347a7c94ffe58177eb72e94e74cd63a2f7
ankita-naik <ankita.naik@ibm.com>
2025-08-23 17:44:40 -0400

    Update collate.py script based on Digit run

--- collate.py
+++ collate.py
@@ -1,14 +1,21 @@
 import os
 import json
 import argparse
-from pathlib import Path
 from tqdm import tqdm
+
+"""
+Usage:
+    PYTHONPATH=./ python collate.py --domains_folder /proj/m3benchmark/dgt/821/raw --balanced --output_folder /proj/m3benchmark/m3data --version v1
+"""
+
 
 def get_all_domains(domain_folder):
     return [name for name in os.listdir(domain_folder) if os.path.isdir(os.path.join(domain_folder, name))]
 
-def load_and_update_samples(domain_list, domain_folder, version, balanced):
-    all_samples = []
+def load_and_update_samples(domain_list, domain_folder, output_path, red_domains, version, balanced):
+    data_unbalanced=[]
+    data_balanced = []
+    unprocessed_domains=0
     sample_counter = 0
     for domain in tqdm(domain_list):
         try:
@@ -20,7 +27,8 @@ def load_and_update_samples(domain_list, domain_folder, version, balanced):
         except:
             json_path = os.path.join(domain_folder, domain, f'multi_turn_{version}.jsonl')
             if not os.path.exists(json_path):
-                print(f"Warning: {json_path} does not exist. Skipping.")
+                unprocessed_domains+=1
+                print(f"Warning: Data for {domain} does not exist. Skipping.")
                 continue
             with open(json_path, 'r') as f:
                 try:
@@ -28,69 +36,75 @@ def load_and_update_samples(domain_list, domain_folder, version, balanced):
                 except json.JSONDecodeError:
                     print(f"Error decoding JSON in {json_path}. Skipping.")
                     continue
-        domain_data = []
+        domain_data_balanced = []
+        domain_data_unbalanced = []        
         for sample in data:
+            # TODO : This fix should be moved to data generation file
             if "types" in sample:
                 sample['type'] = sample.pop('types')
+
+            # Create balanced version
             if balanced:
-                if (("RAG" in sample["type"]) or ("API-API" in sample["type"] and domain != "simpson_episodes")):
+                if ("RAG" in sample["type"]) or ("API-API" in sample["type"]):
+                # if (("RAG" in sample["type"]) or ("API-API" in sample["type"] and domain != "simpson_episodes")): # As there are more domains than simpson_episodes which have high sample size
                     sample['sample_id'] = sample_counter
-                    all_samples.append(sample)
-                    domain_data.append(sample)
-                    sample_counter += 1
-            else:
-                sample['sample_id'] = sample_counter
-                all_samples.append(sample)
-                domain_data.append(sample)
-                sample_counter += 1
+                    data_balanced.append(sample)
+                    domain_data_balanced.append(sample)
+            sample['sample_id'] = sample_counter
+            data_unbalanced.append(sample)
+            domain_data_unbalanced.append(sample)
+            sample_counter += 1 # Global sample counter for both balanced and unbalanced files so that these could be merged in the future
 
-        os.makedirs("../api-sequence-test/balanced/domain_files", exist_ok=True)
-        os.makedirs("../api-sequence-test/unbalanced/domain_files", exist_ok=True)
-
+        if domain in red_domains:
+            unprocessed_domains+=1
+            with open(f"{output_path}/excluded_unbalanced/{domain}_multiturn_bird.json", 'w') as f:
+                json.dump(domain_data_unbalanced, f, indent=2)
+            continue
         if balanced:
-            with open(f"../api-sequence-test/balanced/domain_files/{domain}_multiturn_bird.json", 'w') as f:
-                json.dump(domain_data, f, indent=2)
-        else:
-            with open(f"../api-sequence-test/unbalanced/domain_files/{domain}_multiturn_bird.json", 'w') as f:
-                json.dump(domain_data, f, indent=2)
+            with open(f"{output_path}/balanced/{domain}_multiturn_bird.json", 'w') as f:
+                json.dump(domain_data_balanced, f, indent=2)
+        with open(f"{output_path}/unbalanced/{domain}_multiturn_bird.json", 'w') as f:
+            json.dump(domain_data_unbalanced, f, indent=2)
 
-    return all_samples
+    return data_balanced, data_unbalanced, unprocessed_domains
 
 def main(domains_path, output_path, save_type, version, balanced):
     # domains = get_all_domains(domains_path)
-    domains = [
-    "address", "airline", "authors", "books", "disney", "european_football_1", "movie_platform", 
-    "olympics", "professional_basketball", "university", "video_games", "world",
-    "movielens", "mondial_geo", "legislator", "regional_sales", "world_development_indicators", 
-    "food_inspection_2", "citeseer", "computer_student", "college_completion", 
-    "synthea", "book_publishing_company", "trains", "soccer_2016", "law_episode", 
-    "food_inspection", "european_football_1", "mental_health_survey", "hockey", 
-    "public_review_platform", "retail_complains", "ice_hockey_draft", "menu", "cs_semester", 
-    "beer_factory", "cars", "genes", "shakespeare", "image_and_language",
-    "disney", "music_tracker", "works_cycles", "books", "social_media", 
-    "superstore", "address", "chicago_crime", "professional_basketball", "coinmarketcap", 
-    "movies_4", "app_store", "craftbeer", "movielens",
-    "music_platform_2", "shooting", "car_retails", "airline", "human_resources", 
-    "student_loan", "codebase_comments", "language_corpus", "cookbook", "software_company", 
-    "authors", "shipping", "video_games", "sales", "olympics", "university", 
-    "simpson_episodes", "sales_in_weather", "movielens", "restaurant", "retail_world"
-    ]
-    samples = load_and_update_samples(domains, domains_path, version, balanced)
 
-    os.makedirs(output_path, exist_ok=True)
+    # All domains in BIRD dataset
+    domains=["california_schools", "card_games", "codebase_community", "debit_card_specializing", "european_football_2", "financial", "formula_1", "student_club", "superhero", "thrombosis_prediction", "toxicology"
+             ,"music_platform_2", "shooting", "car_retails", "airline", "human_resources", "student_loan", "codebase_comments", "language_corpus", "bike_share_1", "cookbook", "software_company", "donor", "authors"
+             , "shipping", "video_games", "sales", "olympics", "university", "talkingdata", "simpson_episodes", "movielens", "mondial_geo", "legislator", "regional_sales", "world_development_indicators", "food_inspection_2"
+             , "retail_world", "citeseer", "computer_student", "college_completion", "synthea", "book_publishing_company", "trains", "retails", "soccer_2016", "law_episode", "food_inspection", "european_football_1", "mental_health_survey"
+             , "hockey", "public_review_platform", "retail_complains", "ice_hockey_draft", "menu", "cs_semester", "beer_factory", "cars", "genes", "shakespeare", "image_and_language", "disney", "music_tracker", "works_cycles"
+             , "movie_platform", "books", "social_media", "restaurant", "superstore", "address", "chicago_crime", "professional_basketball", "coinmarketcap", "movies_4", "sales_in_weather", "app_store", "craftbeer", "movie", "world","movie_3"]
+    
+    # Domains to exclude for personal information
+    red_domains=["car_retails", "synthea", "shipping", "cs_semester", "food_inspection_2"
+                 , "sales", "software_company", "social_media", "human_resources", "regional_sales"
+                 ,"works_cycle", "retails", "retail_world", "retail_complains", "shooting", "superstore","movie_3"]
 
-    save_file = os.path.join(output_path, f"{save_type}_{version}.json")
+    os.makedirs(f"{output_path}/balanced/", exist_ok=True)
+    os.makedirs(f"{output_path}/unbalanced/", exist_ok=True)
+    os.makedirs(f"{output_path}/excluded_unbalanced/", exist_ok=True)
 
-    with open(save_file, 'w') as f:
-        json.dump(samples, f, indent=2)
-    print(f"Saved {len(samples)} samples to {save_file}")
+    samples_balanced, samples_unbalanced, unprocessed_domains = load_and_update_samples(domains, domains_path, output_path, red_domains, version, balanced)
+
+    # save_file = os.path.join(output_path, f"{save_type}_{version}.json")
+
+    # with open(save_file, 'w') as f:
+    #     json.dump(samples, f, indent=2)
+    print(f"Saved {len(samples_balanced)} data points in balanced file.")
+    print(f"Saved {len(samples_unbalanced)} data points in unbalanced file.")
+    print(f"Saved {len(red_domains)} domains in excluded_unbalanced folder.")
+    print(f"In total {unprocessed_domains} domains were excluded.")    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--domains_folder', required=True, help="Path to folder with domain-named subfolders")
     parser.add_argument('--balanced', action='store_true', help="Create the balanced version of data")
     parser.add_argument('--output_folder', required=True, help="Path to save the output folder")
-    parser.add_argument('--save_type', choices=['train', 'dev', 'seen', 'unseen', 'full'], required=True, help="Save as full.json, train.json, seen.json etc.")
+    parser.add_argument('--save_type', choices=['train', 'dev', 'seen', 'unseen', 'full'], required=False, help="Save as full.json, train.json, seen.json etc.")
     parser.add_argument('--version', default="v4", help="Version of the dataset (default: v1)")
     args = parser.parse_args()
     main(args.domains_folder, args.output_folder, args.save_type, args.version, args.balanced)
